726ed6af78294f048717f342e63f3864cbf3cf40
tomashonsa <152276276+tomashonsa@users.noreply.github.com>
2024-03-19 13:45:24 +0100

    Update pole.py

--- pole.py
+++ pole.py
@@ -1,10 +1,19 @@
-cislo = int(5)
 cislofloat = float(5.35)
 text = str("ahoj")
 boolean = True 
-
+y = str
 
 arrays = ["ford", "Mustang", "BMW", "Porshe", "audi"]
 
-for x in arrays:
-    print(x)
+
+
+
+
+
+
+for y in  range(len(arrays)):
+     k = 1
+     while k < 6:
+         k + 1
+         print(k, y)
+         k += 1
